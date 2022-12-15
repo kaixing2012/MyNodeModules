63d696ea0eb908e2262ae691ab654497d9ed1224
Edward Y. Rogers <edwardyrogers@EYRs-M1-Pro.local>
2022-12-15 18:43:13 +0800

    update

---
+++ dist/index.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.say = void 0;
+function say(word) {
+    if (!word) {
+        return 'word is not given';
+    }
+    else {
+        return word;
+    }
+}
+exports.say = say;
